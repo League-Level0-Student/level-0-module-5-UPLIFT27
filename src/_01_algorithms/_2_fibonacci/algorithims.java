3fe2d0a1c2be43862f6b0e9f07c20082047a0651
root <root@966a7ec2c7e0>
2023-03-09 03:57:10 +0000

    continue goofy names

---
+++ src/_01_algorithms/_2_fibonacci/algorithims.java
@@ -0,0 +1,51 @@
+package _01_algorithms._2_fibonacci;
+
+import javax.swing.JOptionPane;
+
+public class algorithims {
+
+	
+
+
+		public static void main(String[] args){
+
+		int number1=0;
+		int number2=1;
+		int sum;
+		
+		System.out.println("0");
+		System.out.println("1");
+		for(int i=0; i<10; i++)
+		{
+			sum= number1+number2;
+			System.out.println(sum);
+			number1=number2;
+			number2=sum;
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		}
+		
+		}
--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,17 +11,28 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+String name = JOptionPane.showInputDialog("What is your name");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
+System.out.println(name.toUpperCase());
 
 		// 3. Loop through each character of the name (steps 4 - 7).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
 				// 4. Create a char variable to store the next character of the name
-				//    use .charAt()
+				//    use .charAt
+int nameint = name.length();
+
 	
-				
+		for(int i=0; i<=nameint; i++)	
+		{
+			if(nameint%i==0)
+			{
+				String L = name.charAt(i);
+			}
+		
+		
+		}
 				// 5. Use MODULO operator (%) to identify if it is an EVEN or ODD character.
 			
 			
