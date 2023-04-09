735068b55e891cc4080a4e4a8d22c62d7d340724
root <root@79e3021708fc>
2023-04-09 19:55:14 +0000

    continue Skills practice

--- src/_03_method_writing/_4_calculator/Calculator.java
+++ src/_03_method_writing/_4_calculator/Calculator.java
@@ -11,14 +11,33 @@ public class Calculator {
 		String input2 = JOptionPane.showInputDialog("Enter another number");
 		int number1 = Integer.parseInt(input1);
 		int number2 = Integer.parseInt(input2);
-		
+		int answer=0;
 	
 		int task = JOptionPane.showOptionDialog(null, "What do you choose?", "Calculator", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "divide", "multiply", "subtract", "add" }, null);
 
 		// 3) Call the correct method depending on what option the user chooses
-		System.out.print(multiply(number1, number2));
-		System.out.print(divided(number1, number2));
+	
+		if(task==0)
+		{
+		answer= (divided(number1, number2));
+		}
+		if(task==1)
+		{
+		answer= multiply(number1, number2);
+		}
+		
+		if(task==2)
+		{
+			answer = subtract(number1, number2);
+		}
+		if(task==3)
+			answer = add(number1, number2);
+			
+		
+		JOptionPane.showMessageDialog(null,result(answer));
+		
+		
 		// 4) Call the result() method and put the answer in a pop-up
 	}
 	
@@ -49,9 +68,16 @@ public static int divided(int a, int b)
 {
 
 	return a/b;
+	
 }
 
+public static String result(int answer)
+ {
 
+	
+	return "Your answer is" + answer ;
+	
+}
 
 
 
---
+++ src/_04_practice/_1_skill_practice/Skillspractice.java
@@ -0,0 +1,65 @@
+package _04_practice._1_skill_practice;
+
+import javax.swing.JOptionPane;
+
+public class Skillspractice {
+
+	public static void main(String[] args) {	
+	
+	
+
+		
+		String Dime = JOptionPane.showInputDialog( "How many dimes do you have");
+		int money = Integer.parseInt(Dime);
+		
+	int answer = total(money, 10);
+	
+	JOptionPane.showMessageDialog(null, answer(answer));
+	
+	
+	String height = JOptionPane.showInputDialog("how tall are you in inches");
+	
+	int tall = Integer.parseInt(height);
+	
+	if(tall<=36)
+	{
+		JOptionPane.showMessageDialog(null, "eat some wheaties");
+	}
+	else
+	{
+		JOptionPane.showMessageDialog(null, "Good");
+	}
+	
+	
+	
+	for(int in=0; in<=30; in++)
+	{
+		if(in%3==0)
+		System.out.print(in+ " ");
+	}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+public static int total(int a , int b)
+{
+	return a*b;
+}
+public static String answer(int answer)
+{
+	return "you have "+ answer + " cents";
+}
+
+
+
+
+}
