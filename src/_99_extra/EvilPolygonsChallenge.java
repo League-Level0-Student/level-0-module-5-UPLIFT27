11ffbfa6713f88d92120b5742772bb18d090779c
root <root@1ce50eb02460>
2023-04-28 01:30:03 +0000

    Continue Evil Polygons

--- src/_99_extra/EvilPolygonsChallenge.java
+++ src/_99_extra/EvilPolygonsChallenge.java
@@ -1,4 +1,5 @@
 package _99_extra;
+
 import java.awt.Color;
 
 import javax.swing.JOptionPane;
@@ -6,24 +7,79 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
+	static Robot rob = new Robot();
+
 	public static void main(String[] args) {
-		
-		//1. Create a new Robot
 
-		//2. Set the speed to 100
+		// 1. Create a new Robot
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
-		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
-		//6. Make it so your shapes do not overlap
+		// 2. Set the speed to 100
+		rob.setSpeed(100);
 
-		//7. Challenge: add more colors to the Option Dialog.
+		int colorChoice = JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE,
+				JOptionPane.DEFAULT_OPTION, null, new String[] { "Red", "Green", "Blue", "Purple", "Yellow" }, 0);
+
+		// 3. Use if statements to check the the value of colorChoice and set the pen
+		// color accordingly
+		if (colorChoice == 1) {
+			rob.setPenColor(1000, 0, 0);
+			;
+		}
+		if (colorChoice == 2) {
+			rob.setPenColor(0, 1000, 0);
+		}
+		if (colorChoice == 3) {
+			rob.setPenColor(0, 0, 1000);
+		}
+
+		if (colorChoice == 4) {
+			rob.setPenColor(255, 255, 0);
+		}
+		if (colorChoice == 5) {
+			rob.setPenColor(102, 0, 153);
+		}
+
+		// 4. Ask the use how many polygons they want to be drawn.
+		String i = JOptionPane.showInputDialog("how many polygons do you want to be drawn");
+		String shape = JOptionPane.showInputDialog("what shape do you want drawn");
+		// 5. Use the robot to draw the number of polygons the user requested.
+		int pol = Integer.parseInt(i);
+
+		for (int f = pol; f > 0; f--) {
+			rob.penDown();
+
+			if (shape.equals("square")) {
+				square();
+			}
+
+			else if (shape.equals("triangle")) {
+
+			}
+
+			rob.penUp();
+			rob.move(105);
+
+		}
 	}
-}
 
+	public static void square() {
+		for (int j = 4; j > 0; j--) {
+
+			rob.turn(90);
+			rob.move(100);
+		}
+	}
+
+	public static void traingle() {
+		for (int m = 3; m > 0; m--) {
+			rob.turn(60);
+			rob.move(100);
+
+		}
+
+	}
+	// 6. Make it so your shapes do not overlap
+
+	// 7. Challenge: add more colors to the Option Dialog.
+}
